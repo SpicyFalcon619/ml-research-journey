a3ad9e9570ef5c08519b853964a903dad545c46e
Ahmad Maruf Hossain <hossainmysterio619@gmail.com>
2026-07-01 21:25:29 +0600

    Populate and comment Section 8 Exceptions code

--- Section 8/8.2_exceptions.py
+++ Section 8/8.2_exceptions.py
@@ -1,1 +1,12 @@
-# 8.2 exceptions
+# 8.2 Exceptions
+# Even if a statement is syntactically correct, it may cause an error when executed.
+# These errors are called "Exceptions". Here are a few common ones you will see in ML:
+
+# ZeroDivisionError: division by zero
+# 10 * (1/0)
+
+# NameError: name 'spam' is not defined
+# 4 + spam*3
+
+# TypeError: can only concatenate str (not "int") to str
+# '2' + 2
--- Section 8/8.3_handling_exceptions.py
+++ Section 8/8.3_handling_exceptions.py
@@ -1,1 +1,11 @@
-# 8.3 handling exceptions
+# 8.3 Handling Exceptions
+# In Machine Learning, you will use try/except blocks to catch errors so your script doesn't completely crash.
+# For example, if your code tries to load a corrupted image file, you want to catch the error, log it, and skip it, not crash the whole training loop!
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        print(f"Great! You entered {x}")
+        break
+    except ValueError:
+        print("Oops! That was no valid number. Try again...")
