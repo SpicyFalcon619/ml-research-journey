a0738ac179606d6d7edc31dc0c5de682a4e630bf
Ahmad Maruf Hossain <hossainmysterio619@gmail.com>
2026-07-01 18:07:00 +0600

    Comment docstrings and annotations files

--- Section 4/4.9_More_on_Defining_Functions/4.9.7_documentation_strings.py
+++ Section 4/4.9_More_on_Defining_Functions/4.9.7_documentation_strings.py
@@ -1,1 +1,14 @@
-# 4.9.7 Documentation Strings
+# DOCSTRINGS: A multi-line string right below the function definition that acts as documentation.
+# Whenever you use the built-in help() function on a library, it is reading these docstrings!
+def my_function():
+    """Do nothing, but document it.
+
+    No, really, it doesn't do anything:
+
+        >>> my_function()
+        >>>
+    """
+    pass
+
+# You can access any function's docstring using its special .__doc__ attribute
+print(my_function.__doc__)
