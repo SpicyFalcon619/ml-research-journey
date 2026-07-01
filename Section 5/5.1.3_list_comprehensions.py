ba1245802a3fe3324b86b444037305a7c8129600
Ahmad Maruf Hossain <hossainmysterio619@gmail.com>
2026-07-01 18:59:46 +0600

    Format and comment list comprehensions

--- Section 5/5.1.3_list_comprehensions.py
+++ Section 5/5.1.3_list_comprehensions.py
@@ -1,2 +1,17 @@
-# 5.1.3 List Comprehensions (CRITICAL FOR MACHINE LEARNING!)
-# Example of what you'll learn: squares = [x**2 for x in range(10)]
+# THE OLD WAY (Using a traditional for-loop)
+# This takes 3 lines of code.
+squares1 = []
+for i in range(1, 10):
+    squares1.append(i**2)
+print("Traditional loop:", squares1)
+
+# THE WEIRD WAY (Using map and lambda)
+# This works, but it's a bit messy to read.
+squares2 = list(map(lambda x: x**2, range(1, 10)))
+print("Map and Lambda:", squares2)
+
+# THE PYTHONIC WAY: List Comprehensions (🔥 MUST KNOW 🔥)
+# This is clean, readable, and executes faster than the standard for-loop.
+# Syntax: [ expression for item in iterable ]
+squares3 = [x**2 for x in range(1, 10)]
+print("List Comprehension:", squares3)
